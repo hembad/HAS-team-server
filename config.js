ce22f9c97611b3a6c77f6de398116f8bc6cbd89c
HeMBaD <hemprasad.badgujar@mckinleyrice.com>
2019-04-26 17:08:20 +0530

    Added Configuration

---
+++ config.js
@@ -0,0 +1,31 @@
+
+/**
+ *                                                                                                # 
+ * Copyright (c) 2019 McKinley & Rice, Inc.                                                       # 
+ * This product includes software developed at                                                    # 
+ * The McKinley & Rice, Inc. (https://www.mckinleyrice.com/).                                     # 
+ * This material is proprietary to The McKinley & Rice.                                           # 
+ * All rights reserved. The methods and techniques described                                      # 
+ * herein are considered trade secrets and/or confidential.                                       # 
+ * Reproduction or distribution, in whole or in part, is forbidden                                # 
+ * except by express written permission of The McKinley & Rice.                                   # 
+ *                                                                                                # 
+ * File: c:\Users\HEMBAD\Desktop\new\config.js                                                    # 
+ * Created Date: Friday, April 26th 2019, 3:36:25 pm                                              # 
+ * Author: Hemprasad Badgujar                                                                     # 
+ * -----                                                                                          # 
+ * Last Modified: Friday April 26th 2019 5:07:55 pm                                               # 
+ * Modified By: Hemprasad Badgujar at <admin@mckinleyrice.com>                                    # 
+ * -----                                                                                          # 
+ * HISTORY:                                                                                       # 
+ * Date      	By	Comments                                                                      # 
+ * ----------	---	----------------------------------------------------------                    # 
+ */
+
+
+module.exports = {
+
+    'secret': 'ilovescotchyscotch',
+    'database': 'mongodb://localhost:27017/authtest'
+
+};
